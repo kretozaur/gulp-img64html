4a9d34486f5b42d328487994a22b549d6c3e924b
Marek Marcinkiewicz <marek@Mareks-MBP.home>
2018-01-27 18:45:25 +0100

    add readme.md

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ var mime = require('mime');
 var path = require('path');
 
 module.exports = function(options) {
+
     return through.obj(function(file, enc, callback) {
         
         let output = String(file.contents)
         
-        var $ = cheerio.load(String(file.contents));
+        const $ = cheerio.load(String(file.contents));
         
         $('img').each(function(index, img) {
 
-            var src = $(this).attr("src");
+            const src = $(this).attr("src");
 
             if (src) {
-                var imagePath = path.join(file.base, src);
-                var mimeType = mime.getType(imagePath);
+                const imagePath = path.join(file.base, src);
+                const mimeType = mime.getType(imagePath);
 
                 if (mimeType != 'application/octet-stream') {
        
-                    var base64 = new Buffer(fs.readFileSync(imagePath)).toString('base64');
+                    const base64 = new Buffer(fs.readFileSync(imagePath)).toString('base64');
 
                     output = output.replace(src, `data:${mimeType};base64,${base64}`);
                 }
---
+++ readme.md
@@ -0,0 +1,16 @@
+# gulp-img64-html [![Build Status](https://travis-ci.org/kretozaur/gulp-img64html.png)](https://travis-ci.org/kretozaur/gulp-img64html)
+
+Convert and replace image in HTML to base64.
+
+## Gulp Task:
+
+```js
+var gulp = require('gulp');
+var img64Html = require('gulp-html-img64');
+
+gulp.task('default', function () {
+	gulp.src('index.html')
+	.pipe(img64Html())
+	.pipe(gulp.dest('path'));
+});
+```
